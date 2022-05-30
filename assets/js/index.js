4f207780cac30b94b96f8dbe921ba16cf47a02c8
senyuxi <dajun226@163.com>
2022-05-30 16:00:45 +0800

    完成了登录和注册的功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,62 +1,59 @@
-// 获取用户基本信息
-function getUserInfo () {
-  $.ajax({
-    type: "GET",
-    url: "/my/userinfo",
-    // headers: {
-    //   Authorization: localStorage.getItem("token"),
-    // },
-    success: (res) => {
-      if (res.status !== 0) return layui.layer.msg("数据请求失败！");
-      console.log(res);
-      // 调用 renderAvatar 渲染用户头像
-      renderAvatar(res.data);
-    },
-    // 不论成功还是失败，最终都会调用 complete 回调函数
-    // complete: (res) => {
-    //    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-    //    if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
-    //     //  强制清空 token
-    //     localStorage.removeItem("token");
-    //     // 强制跳转到登录页面
-    //     location.href = "/login.html"
-    //    }
-    // },
-  });
-};
+$(function(){
+    // 点击切换
+    $('#link_reg').click(()=>{
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#link_login').click(()=>{
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
 
-// 渲染用户头像
-const renderAvatar = (user) => {
-  // 获取用户名字
-  let name = user.nickname || user.username;
-  // 设置欢迎文本
-  $("#welcome").html(`欢迎 ${name}`);
-  // 按需渲染用户头像
-  if (user.user_pic !== null) {
-    // 渲染图片头像
-    $(".layui-nav-img").attr("src", user.user_pic).show();
-    $(".text-avatar").hide();
-  } else {
-    // 渲染文本头像
-    $(".layui-nav-img").hide();
-    let firstName = name[0].toUpperCase();
-    $(".text-avatar").html(firstName);
-  }
-};
+    // 引入form模块
+    const form = layui.form
+    // 自定义检测规则
+    form.verify({
+        pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+        repwd:(value)=>{
+            const pwd = $('#form_reg [name=password]').val()
+            if(pwd !== value) return '两次密码不一样'
+        }
+    })
+    // 设置baseurl
+    const layer = layui.layer
+    // 注册功能
+    $('#form_reg').on('submit',(e)=>{
+        e.preventDefault()
+        $.ajax({
+            type:'POST',
+            url: '/api/reguser',
+            data:{
+                username:$('#form_reg [name=username]').val(),
+                password:$('#form_reg [name=password]').val(),
+            },
+            success:(res)=>{
+                if(res.status !== 0) return layer.msg("注册失败")
+                layer.msg('注册成功！')
+                // 模拟点击跳转登录
+                $('#link_login').click()
+            }
+        })
+    })
 
-// 退出登录
-$("#btnLogout").click(() => {
-  layui.layer.confirm(
-    "确定退出登录？",
-    { icon: 3, title: "" },
-    function (index) {
-      // 清空本地存储里面的 token
-      localStorage.removeItem("token");
-      // 重新跳转到登录页面
-      location.href = "/login.html";
-    }
-  );
-});
-
-// 调用 getUserInfo 函数获取用户基本信息
-getUserInfo();
+    // 登录功能
+    $('#form_login').on('submit',function (e){
+        e.preventDefault()
+        $.ajax({
+            type:'POST',
+            url: '/api/login',
+            data:$(this).serialize(),
+            success:(res)=>{
+                if(res.status !== 0) return layer.msg('登陆失败')
+                layer.msg('登录成功')
+                // 登陆成功后，把token令牌存放在本地
+                localStorage.setItem('token',res.token)
+                location.href = '/index.html'
+            }
+        })
+    })
+})
